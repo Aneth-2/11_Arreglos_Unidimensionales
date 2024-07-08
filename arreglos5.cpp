f20126f4bd1d957690d2969b865cdbf13df4f5e7
Aneth-2 <aquispeh@unjbg.edu.pe>
2024-07-08 09:37:26 -0500

    añade arreglos5.cpp

---
+++ arreglos5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int countFP, countPG;
+
+    cout << "Ingrese el numero de estudiantes de Formacion Profesional (FP): ";
+    cin >> countFP;
+
+   
+
+    return 0;
+}
+
