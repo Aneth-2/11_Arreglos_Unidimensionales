74faea553aa9aefd0879f703720ba693c4f19f62
Aneth-2 <aquispeh@unjbg.edu.pe>
2024-07-08 08:56:26 -0500

    se añadio el primer for

--- arreglos3.cpp
+++ arreglos3.cpp
@@ -4,7 +4,12 @@ using namespace std;
 int main() {
     float tempma[7], tempmin[7], tempme, tempb = 999;
     
-
+    for (int i = 0; i < 7; i++) {
+        cout << "Ingrese la menor temperatura del dia " << i + 1 << ": ";
+        cin >> tempmin[i];
+    
+    
+	}
     
     return 0;
 }
