ff621d6a2065d969358d3202506082ac9eab4fdd
Aneth-2 <aquispeh@unjbg.edu.pe>
2024-07-08 08:00:52 -0500

    Añade arreglos2.cpp

--- arreglos2.cpp
+++ arreglos2.cpp
@@ -2,7 +2,13 @@
 using namespace std;
 
 int main() {
-
+	
+    int n;
+    int cantidadPares = 0;
+    int cantidadImpares = 0;
+    
+    cout << "Ingrese la cantidad de numeros en el arreglo: ";
+    cin >> n;
     
     return 0;
 }
