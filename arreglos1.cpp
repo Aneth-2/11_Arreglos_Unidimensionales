ff8812a66771e095a572c5d6a2ef0249f141d930
Aneth-2 <aquispeh@unjbg.edu.pe>
2024-07-08 05:55:52 -0500

    Añade arreglos1.cpp

---
+++ arreglos1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int suma = 0;
+    float contadorCero = 0.0; 
+
+    cout << "Ingrese la cantidad de numeros enteros: ";
+    cin >> n;
+    
+    int* numeros = new int[n]; 
+    
+    
+
+
+    return 0;
+}
+
