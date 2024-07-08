a1260428c88d9d1a5e975327dd37cfccb5fce386
Aneth-2 <aquispeh@unjbg.edu.pe>
2024-07-08 09:29:11 -0500

    añade arreglos4.cpp

---
+++ arreglos4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void relleno(int* arreglo, int tam, int mult);
+
+int main() {
+    int tam, mult;
+    
+    cout << "Ingrese el tamaño del arreglo: ";
+    cin >> tam;
+    
+    cout << "Ingrese el numero para completar con multiplos: ";
+    cin >> mult;
+    
+    
+    return 0;
+}
+
+
+
+
